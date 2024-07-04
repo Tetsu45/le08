d4594c833cd3b55a9cf92d834ebf4ec2ce877990
テイイ <148879729+Tetsu45@users.noreply.github.com>
2024-07-05 08:58:15 +0900

    gg

--- A.cpp
+++ A.cpp
@@ -1,61 +1,64 @@
 #include <iostream>
+using namespace std;
 #include <cstring>
-class BinaryTree{
+class BinaryTree {
     int data;
-    BinaryTree *left,*right;
-    public:
-    BinaryTree(){
+    BinaryTree*left, *right;
+public:
+    BinaryTree() {
         data = 0;
-        *left = *right = NULL;
+        left = right = NULL;
     }
-    ~BinaryTree(){}
+    ~BinaryTree() {}
     BinaryTree(int n);
-    BinaryTree* insert(BinaryTree*,int);
+    BinaryTree* insert(BinaryTree*, int);
     void inOrder(BinaryTree*);
     void preOrder(BinaryTree*);
 };
-BinaryTree::BinaryTree(int n){
-    data =  n;
-    left = NULL;right = NULL;
+BinaryTree::BinaryTree(int n) {
+    data = n;
+    left = NULL; right = NULL;
 }
-BinaryTree* BinaryTree::insert(BinaryTree* root,int value){
-    if(!root){
+BinaryTree* BinaryTree::insert(BinaryTree* root, int value) {
+    if (!root) {
         return new BinaryTree(value);
     }
-    if (value > root->data){
-        root->right = insert(root->right,value);
+    if (value > root->data) {
+        root->right = insert(root->right, value);
     }
-    else if(value < root->data){
-        root->left =  insert(root->left,value)
+    else if (value < root->data) {
+        root->left = insert(root->left, value);
     }
     return root;
 }
-void BinaryTree::inOrder(BinaryTree* tree){
-    if(!tree) return;
+void BinaryTree::inOrder(BinaryTree* tree) {
+    if (!tree) return;
     inOrder(tree->left);
-    cout<< " " << root->data;
+    cout << " " << tree->data;
     inOrder(tree->right);
 }
-void BinaryTree::preOrder(BinaryTree* tree){
-    if(!tree) return;
-    cout << " " <<  tree->data;
+void BinaryTree::preOrder(BinaryTree* tree) {
+    if (!tree) return;
+    cout << " " << tree->data;
     preOrder(tree->left);
     preOrder(tree->right);
 }
-int main(){
-    BinaryTree tree,*root = NULL;
-    char op_name[13];int n;
+int main() {
+    BinaryTree tree, *root = NULL;
+    char op_name[13]; int n;
     std::cin >> n;
-    for (int i = 0; i < n ; i++){
+    for (int i = 0; i < n; i++) {
         std::cin >> op_name;
-        if(strcmp(op_name,"print")== 0){
+        if (strcmp(op_name, "print") == 0) {
             tree.inOrder(root);
+            std::cout << "\n";
             tree.preOrder(root);
+            std::cout << "\n";
         }
         else {
             int val;
             std::cin >> val;
-            tree.insert(root,val);
+            root = tree.insert(root, val);
         }
     }
     return 0;
